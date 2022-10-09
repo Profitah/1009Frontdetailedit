ec27445493ca7fa91969137aa420fb4d7e728e6a
profitah <qwer120542@gmail.com>
2022-10-09 23:12:10 +0900

    프론트엔드 최종

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yuna/Desktop/살려주세요/10-82/myvenv"
+set -gx VIRTUAL_ENV "/Users/yuna/Desktop/이거여줘/wtfstatic/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
